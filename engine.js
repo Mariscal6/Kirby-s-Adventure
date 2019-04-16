d32a622da57239648e897aa0f863a9aa96baef9c
Guille <guills03@ucm.es>
2019-04-16 18:03:32 +0200

    inicio

---
+++ engine.js
@@ -0,0 +1,38 @@
+// ---------------- Engine
+const unroll = (e) => [].concat.apply([], e.map(a => Object.values(a)));
+
+const compiling = {
+  audio: new Array(),
+  sheet: new Array(),
+  level: new Array()
+};
+
+
+const Q = window.Q = Quintus().include("Sprites, Scenes, Anim, 2D, TMX, Input, Audio, Touch, UI").setup({
+    width: 800, // Set the default width to 800 pixels
+    height: 600, // Set the default height to 600 pixels
+}).controls().touch();
+
+
+
+Q.load(unroll(unroll(Object.values(compiling)).map(e => Object.values(e))).join(","), function() {
+
+    // Compile Sprite Sheets
+    compiling.sheet.forEach(sheet => { Q.compileSheets(sheet.png_path, sheet.json_path); });
+
+    // Compile Levels
+    Q.loadTMX(unroll(Object.values(compiling.level)));
+
+    // Compile Sound
+    const assets = Object.assign({}, ...compiling.audio);
+    Q.load(assets);
+    
+    // is Audio
+    if(Q.hasWebAudio){
+        Q.audio.enableWebAudioSound();
+    }else{
+        Q.audio.enableHTML5Sound();
+    }
+
+
+});
---
+++ kirby.js
@@ -0,0 +1,12 @@
+// ----------------- Kirby
+
+/* Load Sound */
+compiling.audio.push({
+    "kirby_run": "./run_kirby.mp3",
+    "kirby_dance": "./kirby_dance.mp3"
+});
+
+
+
+
+
